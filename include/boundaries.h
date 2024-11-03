47789997e03c6f2424ca564b61ff7408e4ab434e
mblancomuriel <manuel.jesus.blanco@gmail.com>
2024-11-03 22:45:54 +0100

    Improve code by transforming default values in static constexpr.

--- include/boundaries.h
+++ include/boundaries.h
@@ -8,17 +8,23 @@ namespace hypl
     class DLL_API Boundaries
     {
     public:
-        Boundaries() :
-            m_xmin {-500.},
-            m_xmax {500.},
-            m_ymin {-100.},
-            m_ymax {700.} {}
+        static constexpr double default_xmin = -500.0;
+        static constexpr double default_xmax = 500.0;
+        static constexpr double default_ymin = -100.0;
+        static constexpr double default_ymax = 700.0;
 
-        Boundaries(double xmin, double xmax, double ymin, double ymax)
-            : m_xmin {xmin},
-              m_xmax {xmax},
-              m_ymin {ymin},
-              m_ymax {ymax} {}
+        Boundaries() :
+            m_xmin{default_xmin}, 
+            m_xmax{default_xmax}, 
+            m_ymin{default_ymin}, 
+            m_ymax{default_ymax} {}
+
+    Boundaries(double xmin, double xmax, double ymin, double ymax) :
+        m_xmin{(xmin <= xmax) ? xmin : xmax},
+        m_xmax{(xmin <= xmax) ? xmax : xmin},
+        m_ymin{(ymin <= ymax) ? ymin : ymax},
+        m_ymax{(ymin <= ymax) ? ymax : ymin} {}
+
 
         //Accessors
         double const& xmin() const {return m_xmin;}
@@ -26,11 +32,15 @@ namespace hypl
         double const& ymin() const {return m_ymin;}
         double const& ymax() const {return m_ymax;}
 
-        //Mutators
-        void set_xmin(double xmin) {m_xmin = xmin;}
-        void set_xmax(double xmax) {m_xmax = xmax;}
-        void set_ymin(double ymin) {m_ymin = ymin;}
-        void set_ymax(double ymax) {m_ymax = ymax;}
+        // Mutators with validation
+        void set_xmin(double xmin) { if (xmin <= m_xmax) m_xmin = xmin; }
+        void set_xmax(double xmax) { if (xmax >= m_xmin) m_xmax = xmax; }
+        void set_ymin(double ymin) { if (ymin <= m_ymax) m_ymin = ymin; }
+        void set_ymax(double ymax) { if (ymax >= m_ymin) m_ymax = ymax; }
+
+        // Utility functions
+        double width() const { return m_xmax - m_xmin; }
+        double height() const { return m_ymax - m_ymin; }        
 
     private:
         double m_xmin;
